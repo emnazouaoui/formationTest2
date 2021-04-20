f34a5af0d2c7960ac991bce21af896dde813c4bb
emnazouaoui <emnazouaoui225@gmail.com>
2021-04-20 16:03:31 +0100

    test1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
-const routes: Routes = [];
+
+import { DomaineUpdateComponent } from './domaine/domaine-update/domaine-update.component';
+import { DomaineListComponent } from './domaine/domaine-list/domaine-list.component';
+
+import { CreateDomaineComponent } from './domaine/create-domaine/create-domaine.component';
+import { DomaineDetailsComponent } from './domaine/domaine-details/domaine-details.component';
+
+import { DomaineTestComponent } from './domaine/domaine-test/domaine-test.component';
+
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'domaines', component: DomaineListComponent },
+  { path: 'updateDomaine/:id', component: DomaineUpdateComponent},
+
+  { path: 'adddomaine', component: CreateDomaineComponent },
+  { path: 'domaine/:id', component: DomaineDetailsComponent },
+  { path: 'testdomaine', component: DomaineTestComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { ProfileComponent } from './profile/profile.component';
+import { authInterceptorProviders } from './_helpers/auth.service';
+
+
+import { DomaineUpdateComponent } from './domaine/domaine-update/domaine-update.component';
+import { DomaineListComponent } from './domaine/domaine-list/domaine-list.component';
+import { DomaineTestComponent } from './domaine/domaine-test/domaine-test.component';
+
+import { CreateDomaineComponent } from './domaine/create-domaine/create-domaine.component';
+import { DomaineDetailsComponent } from './domaine/domaine-details/domaine-details.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+
+
+
+    DomaineUpdateComponent,
+    DomaineListComponent,
+    DomaineTestComponent,
+
+    CreateDomaineComponent,
+    DomaineDetailsComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
